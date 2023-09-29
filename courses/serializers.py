7bc7f953b2434c7b724001be730b447e5f14732e
Jilshak <mohd.jilshak@gmail.com>
2023-09-29 20:42:35 +0530

    changed the serializer of courses

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,7 +3,6 @@ from rest_framework.serializers import ModelSerializer
 from users.serializers import UserSerializer
 
 class CoursesSerializer(ModelSerializer):
-    course_by = UserSerializer()
     class Meta:
         fields = '__all__'
         model = Courses
